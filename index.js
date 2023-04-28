f32e16e6ebb15f961849692946033feb31f7011a
zesanahmed <zesanahmed593@gmail.com>
2023-04-28 22:32:32 +0600

    answer.html page added

--- index.js
+++ index.js
@@ -182,3 +182,7 @@ document.querySelector('#submit').addEventListener("click", () => {
   }, 1500);
   window.scrollTo(0, 0);
 });
+
+document.getElementById('blog').addEventListener('click', function () {
+  window.location.href = "answers.html";
+});
